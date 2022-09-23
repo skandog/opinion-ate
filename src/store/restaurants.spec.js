5d3b47a3a437dd89b3f8ecee6cbdd4c6bf7e3d9a
skandog <muumrar@gmail.com>
2022-09-23 12:45:40 +0100

    setting up store test. written mock api to return hardcoded restaurants, and created a store for the test

---
+++ src/store/restaurants.spec.js
@@ -0,0 +1,28 @@
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import restaurantsReducer from './restaurants/reducers';
+
+describe('restaurants', () => {
+  describe('loadRestaurants action', () => {
+    it('stores the restaurants', async () => {
+      const records = [
+        {id: 1, name: 'Sushi Place'},
+        {id: 2, name: 'Pizza Place'},
+      ];
+
+      const api = {
+        loadRestaurants: () => Promise.resolve(records),
+      };
+
+      const initialState = {
+        records: [],
+      };
+
+      const store = createStore(
+        restaurantsReducer,
+        initialState,
+        applyMiddleware(thunk.withExtraArgument(api)),
+      );
+    });
+  });
+});
